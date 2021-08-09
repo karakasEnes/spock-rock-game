77ab33da8c27e948ae794d7b6980cc0e3c991b54
pyuser <enes.karakas.e@gmail.com>
2021-08-09 17:09:37 +0300

    v3 done, icons prepared, colors done, margin-right fixed

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-
-
 const choices = {
-  rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
-  paper: { name: 'Paper', defeats: ['rock', 'spock'] },
-  scissors: { name: 'Scissors', defeats: ['paper', 'lizard'] },
-  lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
-  spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
+  rock: { name: "Rock", defeats: ["scissors", "lizard"] },
+  paper: { name: "Paper", defeats: ["rock", "spock"] },
+  scissors: { name: "Scissors", defeats: ["paper", "lizard"] },
+  lizard: { name: "Lizard", defeats: ["paper", "spock"] },
+  spock: { name: "Spock", defeats: ["scissors", "rock"] },
 };
 
+const playerContainer = document.querySelector(".player-container");
+
+function playerChoice(e) {
+  console.log(e.target);
+  console.log(e);
+}
+
+playerContainer.addEventListener("click", playerChoice);
